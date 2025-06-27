7f846d511adfddf26ec72ac2f6a5c5e79d2fac60
nhnhmhnm <onep5286@gmail.com>
2025-06-27 17:42:52 +0900

    DivideByZeroException 수정

--- src/main/kotlin/org/example/cal/service/CalculatorService.kt
+++ src/main/kotlin/org/example/cal/service/CalculatorService.kt
@@ -3,6 +3,7 @@ package org.example.cal.service
 import org.example.cal.domain.Operator
 import org.example.cal.dto.CalculatorRequest
 import org.example.cal.dto.CalculatorResponse
+import org.example.cal.exception.DivideByZeroException
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -10,18 +11,15 @@ import java.math.RoundingMode
 @Service
 class CalculatorService {
     fun calculate(request: CalculatorRequest) : CalculatorResponse {
-//        val operand1 = request.operand1
-//        val operand2 = request.operand2
-//        val operator = request.operator
         val (operand1, operand2, operatorStr) = request
-        val operator = Operator.toEnum(operatorStr)
+        val operatorEnum = Operator.toEnum(operatorStr)
 
-        return when (operator) {
+        return when (operatorEnum) {
             Operator.PLUS -> CalculatorResponse(operand1.add(operand2))
             Operator.MINUS-> CalculatorResponse(operand1.subtract(operand2))
             Operator.MULTIPLY -> CalculatorResponse(operand1.multiply(operand2))
             Operator.DIVIDE-> {
-                require(operand2.compareTo(BigDecimal.ZERO) != 0) {"0으로 나눌 수 없습니다."}
+                if (operand2 == BigDecimal.ZERO) throw DivideByZeroException()
                 CalculatorResponse(operand1.divide(operand2, 5, RoundingMode.HALF_UP))
             }
         }

cdb6d664258b4b83c5d2b3dc1932e8d91463c1a2
nhnhmhnm <onep5286@gmail.com>
2025-06-27 13:15:22 +0900

    enum class에 맞게 리펙토링

--- src/main/kotlin/org/example/cal/service/CalculatorService.kt
+++ src/main/kotlin/org/example/cal/service/CalculatorService.kt
@@ -1,9 +1,11 @@
 package org.example.cal.service
 
+import org.example.cal.domain.Operator
 import org.example.cal.dto.CalculatorRequest
 import org.example.cal.dto.CalculatorResponse
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 @Service
 class CalculatorService {
@@ -11,15 +13,16 @@ class CalculatorService {
 //        val operand1 = request.operand1
 //        val operand2 = request.operand2
 //        val operator = request.operator
-        val (operand1, operand2, operator) = request
+        val (operand1, operand2, operatorStr) = request
+        val operator = Operator.toEnum(operatorStr)
 
         return when (operator) {
-            "+" -> CalculatorResponse(operand1 + operand2)
-            "-" -> CalculatorResponse(operand1 - operand2)
-            "*" -> CalculatorResponse(operand1 * operand2)
-            "/" -> {
+            Operator.PLUS -> CalculatorResponse(operand1.add(operand2))
+            Operator.MINUS-> CalculatorResponse(operand1.subtract(operand2))
+            Operator.MULTIPLY -> CalculatorResponse(operand1.multiply(operand2))
+            Operator.DIVIDE-> {
                 require(operand2.compareTo(BigDecimal.ZERO) != 0) {"0으로 나눌 수 없습니다."}
-                CalculatorResponse(operand1 / operand2)
+                CalculatorResponse(operand1.divide(operand2, 5, RoundingMode.HALF_UP))
             }
             else -> throw IllegalArgumentException("Invalid operator")
         }

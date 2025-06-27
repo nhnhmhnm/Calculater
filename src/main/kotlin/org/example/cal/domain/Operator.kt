a3e1374ffd51072130550f4da0aabf1d987c8ecf
nhnhmhnm <onep5286@gmail.com>
2025-06-27 13:14:55 +0900

    operator String -> enum 변환

--- src/main/kotlin/org/example/cal/domain/Operator.kt
+++ src/main/kotlin/org/example/cal/domain/Operator.kt
@@ -1,8 +1,18 @@
 package org.example.cal.domain
 
+import com.fasterxml.jackson.annotation.JsonCreator
+
 enum class Operator(val symbol: String) {
     PLUS("+"),
     MINUS("-"),
     MULTIPLY("*"),
     DIVIDE("/");
+
+    companion object {
+        @JvmStatic // companion object의 요소를 static으로 사용 가능
+        @JsonCreator // json -> enum/객체로 변경
+        fun toEnum(symbol: String): Operator =
+            values().find { it.symbol == symbol }
+                ?: throw IllegalArgumentException("지원하지 않는 연산자입니다: $symbol")
+    }
 }

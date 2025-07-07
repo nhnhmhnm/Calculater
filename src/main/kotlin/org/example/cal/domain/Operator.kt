84dc12db9ecc8c9640dc641ac237b0db565aa41c
nhnhmhnm <onep5286@gmail.com>
2025-07-07 16:09:20 +0900

    CustomException으로 예외 처리

--- src/main/kotlin/org/example/cal/domain/Operator.kt
+++ src/main/kotlin/org/example/cal/domain/Operator.kt
@@ -1,7 +1,8 @@
 package org.example.cal.domain
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import org.example.cal.exception.InvalidOperatorException
+import org.example.cal.exception.CustomException
+import org.example.cal.exception.CustomExceptionWrapper
 
 enum class Operator(val symbol: String) {
     PLUS("+"),
@@ -9,10 +10,11 @@ enum class Operator(val symbol: String) {
     MULTIPLY("*"),
     DIVIDE("/");
 
+    // companion object의 요소를 static으로 사용 -> Operator.toEnum(...)으로 호출
     companion object {
-        @JvmStatic // companion object의 요소를 static으로 사용 가능
+        @JvmStatic // Kotlin -> Java로 사용할 때 static으로 인식되게 (Java와 연동되는 환경에서 씀)
         @JsonCreator // json -> enum/객체로 변경
         fun toEnum(symbol: String) :
-            Operator = entries.find { it.symbol == symbol } ?: throw InvalidOperatorException(symbol)
+            Operator = entries.find { it.symbol == symbol } ?: throw CustomExceptionWrapper(CustomException.INVALID_OPERATOR)
     }
 }
--- src/main/kotlin/org/example/cal/service/CalculatorService.kt
+++ src/main/kotlin/org/example/cal/service/CalculatorService.kt
@@ -3,7 +3,8 @@ package org.example.cal.service
 import org.example.cal.domain.Operator
 import org.example.cal.dto.CalculatorRequest
 import org.example.cal.dto.CalculatorResponse
-import org.example.cal.exception.DivideByZeroException
+import org.example.cal.exception.CustomException
+import org.example.cal.exception.CustomExceptionWrapper
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.math.RoundingMode
@@ -19,7 +20,7 @@ class CalculatorService {
             Operator.MINUS-> CalculatorResponse(operand1.subtract(operand2))
             Operator.MULTIPLY -> CalculatorResponse(operand1.multiply(operand2))
             Operator.DIVIDE-> {
-                if (operand2.compareTo(BigDecimal.ZERO) == 0) throw DivideByZeroException()
+                if (operand2.compareTo(BigDecimal.ZERO) == 0) throw CustomExceptionWrapper(CustomException.DIVIDE_BY_ZERO)
                 CalculatorResponse(operand1.divide(operand2, 10, RoundingMode.HALF_UP))
             }
         }

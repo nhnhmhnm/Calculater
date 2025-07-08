188714fc79b78fe0b34231a0d0c3cb8dfc9af3a6
nhnhmhnm <onep5286@gmail.com>
2025-07-08 15:22:23 +0900

    CalculatorRepository 생성

---
+++ src/main/kotlin/org/example/calculator/repository/CalculatorRepository.kt
@@ -0,0 +1,74 @@
+package org.example.calculator.repository
+
+import org.example.calculator.domain.Calculator
+import org.hibernate.query.sqm.tree.SqmNode.log
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
+import org.springframework.stereotype.Repository
+import java.sql.ResultSet
+
+@Repository
+class CalculatorRepository(
+    val jdbcTemplate : JdbcTemplate
+) {
+    // RowMapper: ResultSet → Calculator 객체로 매핑
+    private val mapper = RowMapper<Calculator> { rs: ResultSet, _ ->
+        Calculator(
+            id = rs.getLong("id"),
+            user_id = rs.getLong("user_id"),
+            operand1 = rs.getBigDecimal("operand1"),
+            operator = rs.getString("operator"),
+            operand2 = rs.getBigDecimal("operand2"),
+            result = rs.getBigDecimal("result"),
+            time = rs.getTimestamp("time").toLocalDateTime()
+        )
+    }
+
+    // 계산 기록 저장
+    fun save(calculator: Calculator): Calculator {
+//        // id 값은 자동 증가로 생략 -> id 값을 모름 -> select로 조회해야 함
+//        val sql = """
+//            INSERT INTO calculations (user_id, operand1, operator, operand2, result, time)
+//            VALUES (?, ?, ?, ?, ?, ?)
+//        """.trimIndent()
+//
+//        jdbcTemplate.update(
+//            sql,
+//            calculator.user_id,
+//            calculator.operand1,
+//            calculator.operator,
+//            calculator.operand2,
+//            calculator.result,
+//            calculator.time
+//        }
+
+        val simpleJdbcOrderInsert = SimpleJdbcInsert(jdbcTemplate).withTableName("calculations").usingGeneratedKeyColumns("id")
+
+        val values = mapOf(
+            "user_id" to calculator.user_id,
+            "operand1" to calculator.operand1,
+            "operator" to calculator.operator,
+            "operand2" to calculator.operand2,
+            "result" to calculator.result,
+            "time" to calculator.time
+        )
+        val newId = simpleJdbcOrderInsert.executeAndReturnKey(values).toLong()
+
+        calculator.id = newId
+        log.info("Generated key insert: $newId")
+
+        return calculator
+    }
+
+    // 특정 사용자(user_id)의 계산 기록 조회 (시간순 내림차순)
+    fun findByUser_Id(user_id: Long): List<Calculator> {
+        val sql = """
+            SELECT * FROM calculations
+            WHERE user_id = ?
+            ORDER BY time DESC
+        """.trimIndent()
+
+        return jdbcTemplate.query(sql, mapper, user_id)
+    }
+}

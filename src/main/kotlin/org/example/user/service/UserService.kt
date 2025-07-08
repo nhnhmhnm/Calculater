4ec8601f03c94446c755fb30f7ca376091866747
nhnhmhnm <onep5286@gmail.com>
2025-07-08 16:45:26 +0900

    excepion 처리

--- src/main/kotlin/org/example/user/service/UserService.kt
+++ src/main/kotlin/org/example/user/service/UserService.kt
@@ -1,5 +1,7 @@
 package org.example.user.service
 
+import org.example.exception.CustomException
+import org.example.exception.CustomExceptionWrapper
 import org.example.user.domain.User
 import org.example.user.dto.CreateUserRequest
 import org.example.user.dto.UpdateUserPasswordRequest
@@ -12,23 +14,47 @@ class UserService(
     private val userRepository: UserRepository
 ) {
     fun createUser(request: CreateUserRequest): UserResponse {
+        // ID 중복 체크
+        if (userRepository.existsByUserID(request.userID)) {
+            throw CustomExceptionWrapper(CustomException.DUPLICATE_USERID)
+        }
+        // PW 체크
+        if (request.userPW.isBlank()) {
+            throw CustomExceptionWrapper(CustomException.INVALID_PASSWORD)
+        }
         val user = User(userID = request.userID, userPW = request.userPW)
 
         return userRepository.save(user).toResponse()
     }
 
-    fun getUser(id: Long): UserResponse =
-        userRepository.findById(id).orElseThrow().toResponse()
+    fun getUser(id: Long): UserResponse {
+        val user = userRepository.findById(id)
+            .orElseThrow { CustomExceptionWrapper(CustomException.USER_NOT_FOUND) }
+
+        return user.toResponse()
+    }
 
     fun updateUserPassword(id: Long, request: UpdateUserPasswordRequest): UserResponse {
-        val user = userRepository.findById(id).orElseThrow()
-        val updated = user.copy(userPW = request.newPassword)
+        val user = userRepository.findById(id)
+            .orElseThrow { CustomExceptionWrapper(CustomException.USER_NOT_FOUND) }
 
+        if (user.userPW != request.currentPassword) {
+            throw CustomExceptionWrapper(CustomException.INVALID_PASSWORD)
+        }
+
+        if (request.newPassword.isBlank()) {
+            throw CustomExceptionWrapper(CustomException.INVALID_PASSWORD)
+        }
+
+        val updated = user.copy(userPW = request.newPassword)
         return userRepository.save(updated).toResponse()
     }
 
     fun deleteUser(id: Long) {
-        userRepository.deleteById(id)
+        val user = userRepository.findById(id)
+            .orElseThrow { CustomExceptionWrapper(CustomException.USER_NOT_FOUND) }
+
+        userRepository.delete(user)
     }
 
     private fun User.toResponse() = UserResponse(id = id,  userID = userID, userPW = userPW)

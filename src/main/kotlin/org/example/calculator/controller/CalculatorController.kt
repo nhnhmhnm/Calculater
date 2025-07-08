cd146ff1799c511c8813369e0d95a4b9cddb274e
nhnhmhnm <onep5286@gmail.com>
2025-07-08 15:21:43 +0900

    userID 추가 & 리팩토링

---
+++ src/main/kotlin/org/example/calculator/controller/CalculatorController.kt
@@ -0,0 +1,21 @@
+package org.example.calculator.controller
+
+import org.example.calculator.dto.CalculatorLogResponse
+import org.example.calculator.dto.CalculatorRequest
+import org.example.calculator.dto.CalculatorResponse
+import org.example.calculator.service.CalculatorService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/calculator")
+class CalculatorController (
+    private val calculatorService: CalculatorService
+) {
+//    @Autowired
+//    private lateinit var calculatorService: CalculatorService
+
+    @PostMapping("/{userID}")
+    fun calculate(@PathVariable userID: Long, @RequestBody request: CalculatorRequest): CalculatorResponse {
+        return calculatorService.calculate(userID, request)
+    }
+}

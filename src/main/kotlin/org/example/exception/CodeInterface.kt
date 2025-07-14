139ab6c3e01fdee31deeb37794ab185ef5b0e6d5
nhnhmhnm <onep5286@gmail.com>
2025-07-14 21:08:01 +0900

    CodeInterface 삭제

--- src/main/kotlin/org/example/exception/CodeInterface.kt
+++
@@ -1,7 +0,0 @@
-package org.example.exception
-
-interface CodeInterface {
-    val status: Int                  // http status code
-    val code: String               // error code
-    val message: String         // error message
-}
--- src/main/kotlin/org/example/exception/CustomException.kt
+++ src/main/kotlin/org/example/exception/CustomException.kt
@@ -1,16 +1,18 @@
 package org.example.exception
 
+import org.springframework.http.HttpStatus
+
 enum class CustomException(
-    override val status: Int,
-    override val code: String,
-    override val message: String
-) : CodeInterface {
+    val status: HttpStatus,
+    val code: String,
+    val message: String
+)  {
     // calculator exception
-    DIVIDE_BY_ZERO(400, "C001", "Can not divide by zero"),
-    INVALID_OPERATOR(400, "C002", "Invalid operator"),
+    DIVIDE_BY_ZERO(HttpStatus.BAD_REQUEST, "C001", "Can not divide by zero"),
+    INVALID_OPERATOR(HttpStatus.BAD_REQUEST, "C002", "Invalid operator"),
 
     // user exception
-    USER_NOT_FOUND(404, "U001", "User not found"),
-    DUPLICATE_USERID(400, "U002", "This ID is duplicated."),
-    INVALID_PASSWORD(400, "U003", "Invalid password"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "User not found"),
+    DUPLICATE_USERID(HttpStatus.BAD_REQUEST, "U002", "This ID is duplicated."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "U003", "Invalid password"),
 }
